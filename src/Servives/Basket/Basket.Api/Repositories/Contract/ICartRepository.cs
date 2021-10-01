31d282693b2f5254e16ab00fc7703a8349a3d80f
Gbogodor Joshua <joshuagbogodor@gmail.com>
2021-10-01 01:20:36 +0100

    Created the repositories for the cart Api microservices

---
+++ src/Servives/Basket/Basket.Api/Repositories/Contract/ICartRepository.cs
@@ -0,0 +1,15 @@
+﻿using Cart.Api.Entites;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Cart.Api.Repositories.Contract
+{
+    public interface ICartRepository
+    {
+        Task<ShoppingCart> GetCart(string userName);
+        Task<ShoppingCart> UpdateCart(ShoppingCart basket);
+        Task DeleteCart(string userName);
+    }
+}
---
+++ src/Servives/Basket/Basket.Api/Repositories/Implementation/CartRepository.cs
@@ -0,0 +1,43 @@
+﻿using Cart.Api.Entites;
+using Cart.Api.Repositories.Contract;
+using Microsoft.Extensions.Caching.Distributed;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Cart.Api.Repositories.Implementation
+{
+    public class CartRepository : ICartRepository
+    {
+        private readonly IDistributedCache _redisCache;
+
+        public CartRepository(IDistributedCache redisCache)
+        {
+            _redisCache = redisCache ?? throw new ArgumentNullException(nameof(redisCache));
+        }
+
+        public async Task<ShoppingCart> GetCart(string userName)
+        {
+            var cart = await _redisCache.GetStringAsync(userName);
+
+            if (String.IsNullOrEmpty(cart))
+                return null;
+
+            return JsonConvert.DeserializeObject<ShoppingCart>(cart);
+        }
+
+        public async Task<ShoppingCart> UpdateCart(ShoppingCart cart)
+        {
+            await _redisCache.SetStringAsync(cart.UserName, JsonConvert.SerializeObject(cart));
+
+            return await GetCart(cart.UserName);
+        }
+
+        public async Task DeleteCart(string userName)
+        {
+            await _redisCache.RemoveAsync(userName);
+        }
+    }
+}
